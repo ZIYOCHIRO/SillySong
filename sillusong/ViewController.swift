34f330c1b3f2467c19992c01342c7c8702e921e9
10.12 <a10.12@1012demacbook-air.local>
2018-07-23 17:49:45 +0800

    Initial Commit

---
+++ sillusong/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  sillusong
+//
+//  Created by 10.12 on 2018/7/23.
+//  Copyright © 2018 10.12. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
